8360ee613c4123c708345153612f613f9b0d2e0a
DS101101 <50895061+DS101101@users.noreply.github.com>
2019-05-22 10:50:14 +0530

    Add files via upload

---
+++ user.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 20, 2019 at 12:02 PM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `credit` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`name`, `email`, `credit`) VALUES
+('aham', 'aham@gmail.com', '59'),
+('ahana', 'ahana@gmail.com', '79'),
+('Akash', 'Akash@gmail.com', '-177'),
+('anand', 'anand@gmail.com', '91'),
+('hari', 'hari@gmail.com', '-76'),
+('ishan', 'ishan@gmail.com', '204'),
+('lavina', 'lavina@gmail.com', '190'),
+('nisha', 'nisha@gmail.com', '142'),
+('nishi', 'nishi@gmail.com', '198'),
+('shreya', 'shreya@gmail.com', '290');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`email`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
